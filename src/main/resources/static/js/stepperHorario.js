593f18d527f637cc49aa91f737191928fa9d67ec
Andrei Mesén Romero <mrandrei.dev@gmail.com>
2026-07-30 00:19:57 -0600

    Se continua con los estilos de cada uno de los stepper para la reserva de citas, los 2 primeros ya están listos estéticamente; se les agregó cierta funcionalidad de validación, pero aún falta mostrar el mensaje cuando el usuario no seleccione algún card.

--- src/main/resources/static/js/stepperHorario.js
+++ src/main/resources/static/js/stepperHorario.js
@@ -11,10 +11,28 @@ document.getElementById('btnSiguiente').addEventListener('click', (e)=> {
     const stepperActive = document.getElementById('stepperActive');
     if(stepperActive.value < 1) stepperActive.value = 1;
     if(stepperActive.value > 4) stepperActive.value = 4;
-    if(stepperActive.value < 4) stepperActive.value = parseInt(stepperActive.value) + 1;
-    moverStepper(stepperActive.value, 'next');
-    moverStepperContent(stepperActive.value, 'next');
+    if(stepperActive.value == 4) return;
+    
+    if(currentStepValid(stepperActive.value) || stepperActive.value == 3) {
+        if(stepperActive.value < 4) stepperActive.value = parseInt(stepperActive.value) + 1;
+        moverStepper(stepperActive.value, 'next');
+        moverStepperContent(stepperActive.value, 'next');
+    }else {
+        // usar el contenedor padre para buscar el párrado de mensaje para que este proceso sea más dinámico
+        const stepperPanelFooterMessage = document.querySelector('.stepper__panel-footer-message');
+        stepperPanelFooterMessage.classList.add('stepper__panel-footer-message--visible');
+
+        // const stepperPanelFooterMessage = document.querySelector('.stepper__panel-footer-message');
+        // stepperPanelFooterMessage.classList.remove('stepper__panel-footer-message--visible');
+    }
 });
+
+function currentStepValid(stepperActive) {
+    const stepperPanelList = document.querySelectorAll('.stepper__content .stepper__panel');
+    const currentStepper = stepperPanelList[stepperActive - 1];
+    const parentStepperPanel = currentStepper.closest('.stepper__panel');
+    return parentStepperPanel.getAttribute('data-valid') == 'true';
+}
 
 function moverStepper(stepperActive, typeMove) {
     const iconList = {
@@ -93,4 +111,60 @@ function moverStepperContent(stepperActive, typeMove) {
     const currentStepperPanel = stepperPanelList[currentStepperPanelNumber];
     stepperPanelToMove.classList.add('stepper__panel--active');
     currentStepperPanel.classList.remove('stepper__panel--active');
+}
+
+const cardsContainerEspecialidades = document.getElementById('containerEspecialidades');
+const cardsEspecialidades = cardsContainerEspecialidades.querySelectorAll('.card__item');
+cardsEspecialidades.forEach(card => {
+    card.addEventListener('click', ()=> {
+        selectCard(cardsEspecialidades, card);
+        cardSelectedUpdateValue(card);
+        
+        const paredStepperPanel = cardsContainerEspecialidades.closest('.stepper__panel');
+        paredStepperPanel.setAttribute('data-valid', 'true');
+    })
+});
+
+const cardsContainerMedicos = document.getElementById('containerMedicos');
+const cardsMedicos = cardsContainerMedicos.querySelectorAll('.card__item');
+cardsMedicos.forEach(card => {
+    card.addEventListener('click', ()=> {
+        selectCard(cardsMedicos, card);
+        cardSelectedUpdateValue(card);
+
+        const paredStepperPanel = cardsContainerMedicos.closest('.stepper__panel');
+        paredStepperPanel.setAttribute('data-valid', 'true');
+    })
+});
+
+// Esta función se llama al momento de renderizar los cards
+function selectCard(listCard, cardSelected) {
+    listCard.forEach(card => {
+        card.classList.remove('card__item--selected');
+    });
+
+    cardSelected.classList.add('card__item--selected');
+}
+
+function cardSelectedUpdateValue(card) {
+    const dataTypeCard = card.getAttribute('data-type-card');
+    let cardValue = 0;
+
+    switch (dataTypeCard) {
+        case 'especialidad':
+            cardValue = parseInt(card.getAttribute('data-especialidad'));
+            document.getElementById('idEspecialidad').value = cardValue;
+            document.getElementById('idEspecialidad').setAttribute('data-especialidad', cardValue)
+            break;
+            
+        case 'medico':
+            cardValue = parseInt(card.getAttribute('data-medico'));
+            document.getElementById('idMedico').value = cardValue;
+            document.getElementById('idMedico').setAttribute('data-medico', cardValue)
+            break;
+
+        default:
+            cardValue = -1;
+            break;
+    }
 }
